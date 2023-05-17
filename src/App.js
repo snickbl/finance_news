33bf9bb9dd32c67385aa9f8c1148b27f11302190
Tofiq <tofiq.alisov.935@gmail.com>
2023-05-18 00:45:57 +0400

    first_update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
 import './App.css';
-import Header from "./Header/Header";
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./Pages/Home";
-import News from "./News/News";
-import { GetData } from "./Components/GetData";
-
 
 function App() {
 
-  GetData()
   
   return (
     <div className="App">
-        <Header/>
-
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/news/:id' element={<News/>}/>
-        </Routes>
+ 
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,33 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from "react-router-dom";
-import { Provider } from 'react-redux';
-import {setupStore} from "./toolkitRedux/index";
-import { GetData } from './Components/GetData';
-
-
-let store = setupStore()
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-      <GetData/>
+
       <App/>
-        {/* <Routes>
-          <Route path='/' element={<App />}/>
-          <Route path='/news/:id' element={<News/>}/>
-        </Routes> */}
-      </BrowserRouter>
-    </Provider>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
